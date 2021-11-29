0793ca538c8715361fa8e5b979c120367656e55f
Johanna Volz Bell Aquino De Gerhardt <j.bell@campus.lmu.de>
2021-11-29 23:04:43 +0100

    Add timeline at the bottom of the map

---
+++ src/components/Timeline.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <HistogramSlider
+
+        :bar-height="50"
+        :bar-width="3"
+        :barRadius="3"
+        :data="data"
+        :prettify="prettify"
+        :drag-interval="true"
+        :force-edges="false"
+        :keyboard="true"
+        :colors="['#FF4B24', '#FF7424']"
+        :primaryColor="'#FF4B24'"
+        :handleColor="'#FF4B24'"
+        :holderColor="'black'"
+        :gridTextColor="'black'"
+        :min="new Date(2004, 11, 24).valueOf()"
+        :max="new Date(2018, 11, 24).valueOf()"
+    />
+  </div>
+</template>
+
+<script>
+import HistogramSlider from "vue-histogram-slider";
+import "vue-histogram-slider/dist/histogram-slider.css";
+import data from "../data.json";
+
+export default {
+name: "Timeline",
+  components: {
+  HistogramSlider,
+  },
+    data() {
+    return {
+      data: data.map(d => new Date(d)),
+      prettify: function(ts) {
+        return new Date(ts).toLocaleDateString("en", {
+          year: "numeric",
+          month: "short",
+          day: "numeric"
+        });
+      }
+    };
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
