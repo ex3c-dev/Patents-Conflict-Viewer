2cafb6cf5f65f91146ce8b989ad8afa10a2a0445
Johanna Volz Bell Aquino De Gerhardt <j.bell@campus.lmu.de>
2022-01-31 21:12:52 +0100

    Revert "Merge branch 'Divide_Patent_filter' into 'main'"
    This reverts merge request !16

--- src/components/PatentTypeFilter.vue
+++ src/components/PatentTypeFilter.vue
@@ -1,45 +1,10 @@
 <template><div class="patent-type-input">
 
-  Patent Section
-  <v-autocomplete
-      class="filter"
-      v-model="selSections"
-      :items="groupList"
-      label="Select patent section"
-      auto-select-first
-      clearable
-      deletable-chips
-      dense
-      multiple
-      rounded
-      small-chips
-      solo
-  ></v-autocomplete>
-
-  Patent Class
-
-  <v-autocomplete
-      class="filter"
-      v-model="selSections"
-      :items="smallSectionList"
-      label="Select patent class"
-      auto-select-first
-      clearable
-      deletable-chips
-      dense
-      multiple
-      rounded
-      small-chips
-      solo
-  ></v-autocomplete>
-
-  Patent subgroup
-
   <v-autocomplete
       class="filter"
       v-model="selSections"
       :items="sectionList"
-      label="Select patent subgroup"
+      label="Type as many types as you want"
       auto-select-first
       clearable
       deletable-chips
@@ -49,16 +14,11 @@
       small-chips
       solo
   ></v-autocomplete>
-
 </div>
 </template>
 <script>
 
-import types_short from "../types_short.json";
 import typelist from "../types.json";
-import typegroup from "../patentgroups.json";
-
-
 import {bus} from "@/main";
 
 export default {
@@ -69,8 +29,6 @@ export default {
       baseUrl: "http://84.252.122.16:3000/tls209?",
       request: "",
       sectionList: typelist,
-      smallSectionList: types_short,
-      groupList: typegroup,
       selSections: [],
     };
   },
