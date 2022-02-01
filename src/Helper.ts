092932b485bf29bc3689b8b1d4a7e1c820d3a7a3
Tobias Theus <tobiastheus@gmail.com>
2022-02-01 21:33:43 +0100

    Read amount of patents for each country from db and stored in json file. Wrote function to read them as a map.

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+/*import axios from "axios";
 
 let baseUrl = "http://84.252.122.16:3000/geoc_app?"
-
+*/
 const countryList = require('./countryList.json')
+const patentAmountList = require('./patentamount.json')
 
 export default {
     methods: {
@@ -11,16 +12,26 @@ export default {
             console.log(countryList.length)
         },
 
-        getMax: async function() {
+        getMax: function() {
+            let patenMap = new Map()
+
+            patentAmountList.forEach((entry) =>{
+                patenMap[entry.country] = entry.amount
+            })
+            return patenMap
+
+            /* TODO: Deine alte funktion noch caro
+            let text = "[\n"
 
             let offset = 1
             var joinedPatents =  new Map
 
                 let config = {
                 headers: {
-                    "prefer": "count=planned",
+                    "prefer": "count=exact",
                 }
             }
+            console.log(text)
 
             await countryList.forEach((tmp,index) => {
 
@@ -34,9 +45,17 @@ export default {
                             let max = parseInt (response.headers["content-range"].split("/")[1])
                             joinedPatents.set(tmp, max)
                             console.log(tmp + "= " + joinedPatents.get(tmp))
+                            text += "{\"country\": \"" + tmp + "\", \"amount\": " + joinedPatents.get(tmp) + "},\n"
                             //return joinedPatents
                             if(index === countryList.length-1) {
                                 console.log("last")
+                                text += "]"
+                                console.log(text)
+                                fs.writeFileSync("amountPatents.json", text, function (err){
+                                    if (err){
+                                        return console.log("error")
+                                    }
+                                })
                             }
                         }
                     )
@@ -63,6 +82,7 @@ export default {
                 str += ")"
             }
             return str
+            */
         },
     },
 
