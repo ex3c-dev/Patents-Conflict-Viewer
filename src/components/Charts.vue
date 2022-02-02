8aae48916d0b029328f09119bdc3d27eeeef6c1d
NWuest <92848464+NWuest@users.noreply.github.com>
2022-02-02 16:52:58 +0100

    Integrate Bug Fix of Map in Helper.ts

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -19,6 +19,22 @@
         </div>
       </template>
     </vl-overlay>
+
+    <!--
+    <template>
+      <div>
+        <vl-feature
+            v-for="patentCircle in emptyPatentList"
+            :key="patentCircle.id"
+            :id="patentCircle.id">
+          <vl-geom-circle :coordinates="patentCircle.coordinates" :radius="10000"></vl-geom-circle>
+        </vl-feature>
+      </div>
+    </template>
+    -->
+
+
+
   </div>
 </template>
 
@@ -30,6 +46,7 @@ import centralPoints from "../countries.json";
 import VueLayers from "vuelayers";
 import Overlay from "vuelayers";
 import countryList from "../countryList.json"
+import helper from "../Helper.ts"
 
 Vue.use(Overlay)
 
@@ -44,28 +61,26 @@ export default {
   data() {
     return {
       centralPointsList: centralPoints,
+      emptyPatentList: [],
+      sumTitle: "",
       mapValue: [],
       patentData: [],
       testData: new Map,
       selectedCountries: [],
+      maxPatentList: [],
       patentChartList: [],
-      charts: [
-        //{id: "München", coordinates: [11.576, 48.137], radius: 40000, description: ""},
-        //{id: "Köln",coordinates: [6.95, 50.93333], radius: 15000, description: ""}
-      ],
       chartOptions: {
         chart: {
           type: 'pie'
         },
-        /*
         title: {
-          text: 'Patent Types',
+          text: this.sumTitle,
           align: 'center',
           style: {
             fontSize:  '20px',
             color: '#E91E63',
           },
-        },*/
+        },
         legend: {
           show: false
         },
@@ -83,13 +98,12 @@ export default {
           "H: Electricity", "Undefined"],
       },
     }
-
   },
   created() {
     bus.$on('selected-countries', (selCountries) => {
       console.log(selCountries);
       this.selectedCountries = selCountries
-    }),
+    })
     bus.$on('filtered-map', (data) => {
       this.patentData = [];
       data.forEach((value) => {
@@ -101,16 +115,24 @@ export default {
   watch: {
     patentData: async function () {
       this.generateCharts();
-    },
-    testData: async function () {
-      //console.log(this.testData);
     }
   },
   methods: {
-
+    /**
+     * Function returning one patent chart for each country with different patent types
+     */
     generateCharts() {
       console.log("Select Patents");
       this.patentChartList = [];
+      let maxValues = helper.methods.getMax();
+
+      this.maxPatentList = maxValues.entries();
+
+      console.log(maxValues);
+
+
+      //console.log(this.maxPatentList);
+
       if (this.selectedCountries.length === 0) {
         this.selectedCountries = countryList;
       }
@@ -119,7 +141,14 @@ export default {
 
         let filteredPatentCountry = this.patentData.filter(patent => patent.name_0 === country);
         let sum = filteredPatentCountry.length;
+
+
+        //let sumMax = this.maxPatentList[country];
+
         let chartSize;
+
+
+        //console.log(sumMax);
 
         if(sum < 100) {
           chartSize = "50%"
@@ -133,7 +162,7 @@ export default {
           chartSize = "100%"
         }
 
-        console.log("Country: " + country + " Patentanzahl: " + sum);
+        //console.log("Country: " + country + " Amount: " + sum);
 
         let aCounter = 0;
         let bCounter = 0;
@@ -178,23 +207,25 @@ export default {
         })
 
         let actualCountry = this.centralPointsList.find((centerCountry) => (centerCountry.name === country));
-        console.log(actualCountry);
+        //console.log(actualCountry);
 
         if (actualCountry) {
           centralPoint = [actualCountry.longitude, actualCountry.latitude]
           //let sumCounter = aCounter + bCounter + cCounter + dCounter + eCounter + fCounter + gCounter + hCounter;
           let series = [aCounter, bCounter, cCounter, dCounter, eCounter, fCounter, gCounter, hCounter, undefinedTypes];
-          let patentChart = {name: country, coordinates: centralPoint, diameter: chartSize, series: series}
-          console.log(patentChart);
+          let patentChart = {name: country, coordinates: centralPoint, diameter: chartSize, series: series};
+          //console.log(patentChart);
           if(sum > 0) {
             this.patentChartList.push(patentChart);
-          }
+          } /*else {
 
+            console.log("emptyPatent: " + country)
+
+            let emptyPatentCircle = {name: country, coordinates: centralPoint};
+            this.emptyPatentList.push(emptyPatentCircle)
+          }*/
         }
-
       })
-      //console.log(patent);
-
     }
 
   }
